e03917981027c8b016c2f10376f7d13113a55521
Oluwadmilola <mustapharakiyat@gmail.com>
2025-07-02 18:10:59 +0100

    banner added

--- src/page/AboutUs.jsx
+++ src/page/AboutUs.jsx
@@ -1,3 +1,6 @@
+import teams from "../images/team.jpg"
+
+
 const AboutUs = () => {
   return (
     <div className="text-color ms-5 me-5 mr-5 mt-3">
@@ -7,6 +10,25 @@ const AboutUs = () => {
         funds, and access financial tools from anywhere, at any time. Built with cutting-edge cloud technologies,
         CloudForge ensures a seamless and trustworthy digital banking experience.
       </p>
+      <p>
+        Our mission is to empower individuals and businesses with innovative banking solutions that enhance financial
+        freedom and convenience. We prioritize user experience, offering a user-friendly interface, robust security
+        measures, and responsive customer support. Whether you're managing personal finances or running a business,
+        CloudForge Bank is your trusted partner in achieving financial success.
+       </p>
+       <div>
+        <h2>Meet The Team</h2>
+        <img
+          src={teams}
+          alt="Tech-Tribe Banner"
+          width="100%"
+          height="auto"
+          className="home-image"
+        />
+        <p>
+
+        </p>
+       </div>
     </div>
   );
 };
